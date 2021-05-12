cfae342066118550686120455270bc7c0d16af6d
Manoel <manoelll@gmail.com>
2021-05-11 23:49:58 -0300

    Exercício final  modulo 1 CRUD Client

---
+++ exercicioModulo-1/src/main/java/com/bootcamp/exercicio/services/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.bootcamp.exercicio.services.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ exercicioModulo-1/src/main/resources/data.sql
@@ -0,0 +1,42 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ronaldo Alencar Macedo',  '105.183.870-23', 2500, '2000-09-20T10:40:3Z', 3);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Adriana Almeida de Castro', '534.824.680-87', 2500, '1990-03-20T10:35Z', 1);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Thiago de Jesus Dantas', '383.809.340-24', 3900, '2003-04-21T10:10:38Z', 4);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Everaldo de aparecido', '229.845.440-05', 5590, '2011-01-10T10:40:35Z', 5);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria Jose Guimaes', '766.598.040-64', 8300, '2020-01-20T10:40:35Z', 2);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Clara Pedroso', '448.179.340-69', 6298, '2010-12-20T10:40:35Z', 3);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jussara Pimentel da Graça', '305.340.660-89', 1050, '219-05-20T10:40:35Z', 0);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juliano Tomaz Pedrito', '871.555.660-39', 4385, '1998-03-20T10:40:35Z', 6);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fernando do Amaral', '470.411.220-93', 11400, '2010-04-20T10:40:35Z', 1);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Leonel Judas Tadeu da Silva', '762.144.760-52', 2200, '2021-01-20T10:40:35Z', 4);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juliando da Cruz', '762.144.760-52', 2200, '2021-01-20T10:40:35Z', 4);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paulo Eduardo Romero', '308.780.520-38', 2200, '1999-01-20T10:40:35Z', 4);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Kaio Lima de Oliveira', '7115.138.600-62', 1878, '2021-01-18T10:40:35Z', 4);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rafael da Silva', '787.298.650-77', 5800, '2001-05-15T10:40:35Z', 2);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('André Jonas', '579.683.260-36', 1200, '2021-04-22T10:40:35Z', 3);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Camila da Trindade ', '914.933.660-68', 4000, '2021-08-04T10:40:35Z', 8);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Silvana Dutra', '762.144.760-52', 2015, '3200-11-01T10:40:35Z', 9);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Roberto Carlos Silva', '851.965.180-10', 18800, '2021-10-27T10:40:35Z', 10);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Camila Da Silva Paz', '076.818.310-37', 9500, '2021-03-15T10:40:35Z', 15);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jucelino Amado Souza', '700.776.460-61', 5300, '2021-01-8T10:40:35Z', 33);
+ 
+ INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cassandra das Palmeira', '091.308.360-77', 2000, '2021-12-20T10:40:35Z', 22);
+ 
